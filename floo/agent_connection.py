3f046324c294143eb0c78576937793372069169d
Geoff Greer <geoff@greer.fm>
2013-01-20 23:13:13 -0800

    Don't need this anymore

--- floo/agent_connection.py
+++ floo/agent_connection.py
@@ -244,8 +244,6 @@ class AgentConnection(object):
             self.reconnect()
             return
 
-        if not settings.get('run', True):
-            return sublime.set_timeout(self.select, 1000)
         # this blocks until the socket is readable or writeable
         _in, _out, _except = select.select([self.sock], [self.sock], [self.sock])
 
