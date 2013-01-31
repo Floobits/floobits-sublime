c7e598fa774254237029d890df6c05f322d41d77
Geoff Greer <geoff@greer.fm>
2013-01-30 17:04:23 -0800

    Log when we can't load persistent data

--- floo/utils.py
+++ floo/utils.py
@@ -38,10 +38,12 @@ def get_persistent_data():
     try:
         per = open(per_path, 'rb')
     except (IOError, OSError):
+        print('Failed to open %s. Recent room list will be empty.' % per_path)
         return {}
     try:
         persistent_data = json.loads(str(per.read()))
     except:
+        print('Failed to parse %s. Recent room list will be empty.' % per_path)
         return {}
     return persistent_data
 
