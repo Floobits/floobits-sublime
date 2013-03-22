b6047ff2c3ab47cc1be8155de3fce54b800f7162
Geoff Greer <geoff@greer.fm>
2013-03-22 15:48:32 -0700

    If debug is enabled, log ridiculously

--- floo/msg.py
+++ floo/msg.py
@@ -18,8 +18,11 @@ LOG_LEVEL = LOG_LEVELS['MSG']
 
 def get_or_create_chat(cb=None):
     def return_view():
+        global LOG_LEVEL
         G.CHAT_VIEW_PATH = G.CHAT_VIEW.file_name()
         G.CHAT_VIEW.set_read_only(True)
+        if G.DEBUG:
+            LOG_LEVEL = LOG_LEVELS['DEBUG']
         if cb:
             return cb(G.CHAT_VIEW)
 
