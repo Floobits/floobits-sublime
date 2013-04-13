270d9e24aa11eb1a953fa7b7f7caa128c0fb27ff
Geoff Greer <geoff@greer.fm>
2013-04-12 20:12:22 -0700

    Use spaces instead of equals. Settings is global

--- floobits.py
+++ floobits.py
@@ -56,7 +56,7 @@ def load_floorc():
     fd.close()
 
     for setting in default_settings:
-        sep = setting.find('=')
+        sep = setting.find(' ')
         if sep <= 0:
             continue
         name = setting[:sep]
@@ -66,6 +66,7 @@ def load_floorc():
 
 
 def reload_settings():
+    global settings
     print('Reloading settings...')
     default_settings = load_floorc()
     for name, val in default_settings.items():
