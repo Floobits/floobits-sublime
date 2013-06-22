5679955e1700cdfd4c6bb46b95ebccc724c32cea
Geoff Greer <geoff@greer.fm>
2013-06-21 18:16:37 -0700

    Use https basic auth when hitting the API. This fixes a bug where rooms were created secret by default.

--- floo/api.py
+++ floo/api.py
@@ -1,10 +1,12 @@
+import base64
+
 try:
     from urllib.parse import urlencode
-    from urllib.request import urlopen
+    from urllib.request import Request, urlopen
     assert urlencode, urlopen
 except ImportError:
     from urllib import urlencode
-    from urllib2 import urlopen
+    from urllib2 import Request, urlopen
 
 
 try:
@@ -18,8 +20,9 @@ def create_workspace(workspace_name):
     url = 'https://%s/api/workspace/' % G.DEFAULT_HOST
     # TODO: let user specify permissions
     post_data = {
-        'username': G.USERNAME,
-        'secret': G.SECRET,
         'name': workspace_name
     }
-    urlopen(url, data=urlencode(post_data).encode('ascii'), timeout=5)
+    r = Request(url, data=urlencode(post_data).encode('utf-8'))
+    basic_auth = base64.encodestring('%s:%s' % (G.USERNAME, G.SECRET)).replace('\n', '')
+    r.add_header('Authorization', 'Basic %s' % basic_auth)
+    urlopen(r, timeout=5)
