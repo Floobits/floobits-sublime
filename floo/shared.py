212a472694d87f3faa24fe26ae93f06b11253ea8
Geoff Greer <geoff@greer.fm>
2013-07-24 17:00:47 -0700

    Clean up shared.py

--- floo/shared.py
+++ floo/shared.py
@@ -1,44 +1,46 @@
 import os
 
 
+# Protocol version
 __VERSION__ = '0.03'
 
-DEBUG = False
-
-PLUGIN_PATH = None
-
-JOINED_WORKSPACE = False
-AUTO_GENERATED_ACCOUNT = False
-
-BASE_DIR = os.path.expanduser(os.path.join('~', 'floobits'))
-COLAB_DIR = ''
-
-PROJECT_PATH = ''
-DEFAULT_HOST = 'floobits.com'
-DEFAULT_PORT = 3448
-SECURE = True
-
+# Config settings
 USERNAME = ''
 SECRET = ''
 API_KEY = ''
 
+DEBUG = False
+SOCK_DEBUG = False
+
 ALERT_ON_MSG = True
-
-PERMS = []
-
-WORKSPACE_WINDOW = None
-
-CHAT_VIEW = None
-CHAT_VIEW_PATH = None
 LOG_TO_CONSOLE = False
+OBEY_DEFAULT_IGNORES = True
 
+BASE_DIR = os.path.expanduser(os.path.join('~', 'floobits'))
+
+
+# Shared globals
+DEFAULT_HOST = 'floobits.com'
+DEFAULT_PORT = 3448
+SECURE = True
+
+
+COLAB_DIR = ''
+PROJECT_PATH = ''
+JOINED_WORKSPACE = False
+PERMS = []
 STALKER_MODE = False
 
-IGNORE_MODIFIED_EVENTS = False
+AUTO_GENERATED_ACCOUNT = False
+PLUGIN_PATH = None
+WORKSPACE_WINDOW = None
+CHAT_VIEW = None
+CHAT_VIEW_PATH = None
 
 TICK_TIME = 100
 AGENT = None
 
+IGNORE_MODIFIED_EVENTS = False
 VIEW_TO_HASH = {}
 
-FLOORC_PATH = os.path.expanduser('~/.floorc')
+FLOORC_PATH = os.path.expanduser(os.path.join('~', '.floorc'))
