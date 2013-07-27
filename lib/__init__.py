9a68a7db1d9720862b3868b6bbec354e66fbd83a
Geoff Greer <geoff@greer.fm>
2013-07-27 16:42:36 -0700

    Squashed 'floo/common/' changes from 011bb89..c725686
    c725686 Add FlooPatch class to utils. Make it take the current text instead of a view.
    8d70b28 Fix import of DMP.
    b07d5d9 Move msg.py into common stuff.
    
    git-subtree-dir: floo/common
    git-subtree-split: c725686fd88abd8f128947a78454003890c6d7f7

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,13 @@
-from lib import dmp_monkey
+try:
+    from . import dmp_monkey
+except ImportError:
+    import dmp_monkey
+
 dmp_monkey.monkey_patch()
 
-from lib import diff_match_patch
+try:
+    from . import diff_match_patch
+except ImportError:
+    import diff_match_patch
+
 DMP = diff_match_patch.diff_match_patch()
