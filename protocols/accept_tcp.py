31e3ea2a231b34197bd988d780b0c7554e45b95b
Matt Kaniaris <mkaniaris@gmail.com>
2013-10-30 04:40:33 -0700

    now accepting incoming connections

--- protocols/accept_tcp.py
+++ protocols/accept_tcp.py
@@ -9,22 +9,25 @@ except (ImportError, ValueError):
 
 class ListenerProtocol(base.BaseProtocol):
     def __init__(self, host, port):
+        super(ListenerProtocol, self).__init__(host, port, False)
         self.host = host
         self.port = port
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self._sock.bind((host, port))
+        self._sock.listen(1)
 
     def __len__(self):
         return 0
 
     def fileno(self):
-        return self._sock.fileno()
+        return self._sock
 
     def fd_set(self, readable, writeable, errorable):
         readable.append(self._sock)
 
     def read(self):
-        conn, addr = self._sock.accept(1)
+        print("reading")
+        conn, addr = self._sock.accept()
         conn.setblocking(False)
         self.emit("connect", conn, addr)
