d774742689ceff32515ca60f5d1bf3a0c72b68aa
Geoff Greer <geoff@greer.fm>
2013-11-15 14:55:21 -0800

    Squashed 'floo/common/' changes from bc039ff..f07d874
    f07d874 Don't hard-code sublime text as editor/client version/name.
    6e85819 Merge common. Fix conflict.
    
    git-subtree-dir: floo/common
    git-subtree-split: f07d8740ce91c72db2ddca5fc312ab2b6729dd8e

--- api.py
+++ api.py
@@ -32,7 +32,7 @@ def api_request(url, data=None):
     r.add_header('Authorization', 'Basic %s' % get_basic_auth())
     r.add_header('Accept', 'application/json')
     r.add_header('Content-type', 'application/json')
-    r.add_header('User-Agent', 'Floobits Sublime Plugin %s %s py-%s.%s' % (G.__PLUGIN_VERSION__, editor.platform(), sys.version_info[0], sys.version_info[1]))
+    r.add_header('User-Agent', 'Floobits Plugin %s %s %s py-%s.%s' % (editor.name(), G.__PLUGIN_VERSION__, editor.platform(), sys.version_info[0], sys.version_info[1]))
     return urlopen(r, timeout=5)
 
 
