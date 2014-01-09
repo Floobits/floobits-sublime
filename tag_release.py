3d41077e5be794b530bdbdae6c5c31d3b9c7aebd
Geoff Greer <geoff@greer.fm>
2014-01-09 14:30:54 -0800

    Don't parse and stomp over package.json. Just tag when pushing release.

--- tag_release.py
+++ tag_release.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python
 
-import json
-from datetime import datetime
 import os
 import sys
 
 
 def main():
-    with open('packages.json', 'r') as fd:
-        pkg_json = json.loads(fd.read())
-
     if len(sys.argv) != 2:
-        print('Usage: %s %s' % (sys.argv[0], pkg_json['packages'][0]['releases'][0]['version']))
+        print('Usage: %s version' % sys.argv[0])
+        os.system('git tag | sort -n | tail -n 1')
         sys.exit()
 
     version = sys.argv[1]
-
-    now = datetime.now()
-    pkg_json['packages'][0]['releases'][0]['date'] = now.strftime('%Y-%m-%d %H:%M:%S')
-    pkg_json['packages'][0]['releases'][0]['version'] = version
-    pkg_json['packages'][0]['releases'][0]['url'] = 'https://github.com/Floobits/floobits-sublime/archive/%s.zip' % version
-
-    with open('packages.json', 'w') as fd:
-        fd.write(json.dumps(pkg_json, indent=4, separators=(',', ': '), sort_keys=True))
 
     with open('floo/version.py', 'r') as fd:
         version_py = fd.read().split('\n')
