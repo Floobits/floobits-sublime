fe5493c19d34fc14ffd81b7bf8400325d7a25a29
Matt Kaniaris <mkaniaris@gmail.com>
2014-05-22 22:34:01 -0700

    move text commands out

--- floobits.py
+++ floobits.py
@@ -1,11 +1,9 @@
 # coding: utf-8
 import sys
 import os
-import hashlib
 import subprocess
 import threading
 
-import sublime_plugin
 import sublime
 
 PY2 = sys.version_info < (3, 0)
@@ -26,19 +24,20 @@ Please upgrade your operating system if you want to use this plugin. :(''')
 
 try:
     from .floo import version
-    from .floo import sublime_utils as sutils
     from .floo.listener import Listener
     from .floo.common import reactor, shared as G, utils
     from .floo.common.exc_fmt import str_e
     assert utils
 except (ImportError, ValueError):
     from floo import version
-    from floo import sublime_utils as sutils
     from floo.listener import Listener
     from floo.common import reactor, shared as G, utils
     from floo.common.exc_fmt import str_e
 
 reactor = reactor.reactor
+
+from text_commands import FlooViewReplaceRegion, FlooViewReplaceRegions
+assert FlooViewReplaceRegion and FlooViewReplaceRegions
 
 from window_commands import FloobitsOpenSettingsCommand, FloobitsShareDirCommand, FloobitsCreateWorkspaceCommand, \
     FloobitsPromptJoinWorkspaceCommand, FloobitsJoinWorkspaceCommand, FloobitsPinocchioCommand, \
@@ -58,9 +57,6 @@ assert Listener and version and FloobitsOpenSettingsCommand and FloobitsShareDir
     FloobitsOpenWorkspaceSettingsCommand and RequestPermissionCommand and FloobitsFollowSplit and FloobitsNotACommand
 
 
-ignore_modified_timeout = None
-
-
 def ssl_error_msg(action):
     sublime.error_message('Your version of Sublime Text can\'t ' + action + ' because it has a broken SSL module. '
                           'This is a known issue on Linux builds of Sublime Text. '
@@ -78,105 +74,6 @@ def global_tick():
     # XXX: A couple of sublime 2 users have had reactor == None here
     reactor.tick()
     utils.set_timeout(global_tick, G.TICK_TIME)
-
-
-def unignore_modified_events():
-    G.IGNORE_MODIFIED_EVENTS = False
-
-
-def transform_selections(selections, start, new_offset):
-    new_sels = []
-    for sel in selections:
-        a = sel.a
-        b = sel.b
-        if sel.a > start:
-            a += new_offset
-        if sel.b > start:
-            b += new_offset
-        new_sels.append(sublime.Region(a, b))
-    return new_sels
-
-
-# The new ST3 plugin API sucks
-class FlooViewReplaceRegion(sublime_plugin.TextCommand):
-    def run(self, edit, *args, **kwargs):
-        selections = [x for x in self.view.sel()]  # deep copy
-        selections = self._run(edit, selections, *args, **kwargs)
-        self.view.sel().clear()
-        for sel in selections:
-            self.view.sel().add(sel)
-
-    def _run(self, edit, selections, r, data, view=None):
-        global ignore_modified_timeout
-
-        if not hasattr(self, 'view'):
-            return selections
-
-        G.IGNORE_MODIFIED_EVENTS = True
-        utils.cancel_timeout(ignore_modified_timeout)
-        ignore_modified_timeout = utils.set_timeout(unignore_modified_events, 2)
-        start = max(int(r[0]), 0)
-        stop = min(int(r[1]), self.view.size())
-        region = sublime.Region(start, stop)
-
-        if stop - start > 10000:
-            self.view.replace(edit, region, data)
-            G.VIEW_TO_HASH[self.view.buffer_id()] = hashlib.md5(sutils.get_text(self.view).encode('utf-8')).hexdigest()
-            return transform_selections(selections, stop, 0)
-
-        existing = self.view.substr(region)
-        i = 0
-        data_len = len(data)
-        existing_len = len(existing)
-        length = min(data_len, existing_len)
-        while (i < length):
-            if existing[i] != data[i]:
-                break
-            i += 1
-        j = 0
-        while j < (length - i):
-            if existing[existing_len - j - 1] != data[data_len - j - 1]:
-                break
-            j += 1
-        region = sublime.Region(start + i, stop - j)
-        replace_str = data[i:data_len - j]
-        self.view.replace(edit, region, replace_str)
-        G.VIEW_TO_HASH[self.view.buffer_id()] = hashlib.md5(sutils.get_text(self.view).encode('utf-8')).hexdigest()
-        new_offset = len(replace_str) - ((stop - j) - (start + i))
-        return transform_selections(selections, start + i, new_offset)
-
-    def is_visible(self):
-        return False
-
-    def is_enabled(self):
-        return True
-
-    def description(self):
-        return
-
-
-# The new ST3 plugin API sucks
-class FlooViewReplaceRegions(FlooViewReplaceRegion):
-    def run(self, edit, commands):
-        is_read_only = self.view.is_read_only()
-        self.view.set_read_only(False)
-        selections = [x for x in self.view.sel()]  # deep copy
-        for command in commands:
-            selections = self._run(edit, selections, **command)
-
-        self.view.set_read_only(is_read_only)
-        self.view.sel().clear()
-        for sel in selections:
-            self.view.sel().add(sel)
-
-    def is_visible(self):
-        return False
-
-    def is_enabled(self):
-        return True
-
-    def description(self):
-        return
 
 
 called_plugin_loaded = False
---
+++ text_commands.py
@@ -0,0 +1,114 @@
+# coding: utf-8
+import hashlib
+
+import sublime_plugin
+import sublime
+
+try:
+    from .floo import sublime_utils as sutils
+    from .floo.common import shared as G, utils
+    assert utils
+except (ImportError, ValueError):
+    from floo import sublime_utils as sutils
+    from floo.common import shared as G, utils
+
+ignore_modified_timeout = None
+
+
+def unignore_modified_events():
+    G.IGNORE_MODIFIED_EVENTS = False
+
+
+def transform_selections(selections, start, new_offset):
+    new_sels = []
+    for sel in selections:
+        a = sel.a
+        b = sel.b
+        if sel.a > start:
+            a += new_offset
+        if sel.b > start:
+            b += new_offset
+        new_sels.append(sublime.Region(a, b))
+    return new_sels
+
+
+# The new ST3 plugin API sucks
+class FlooViewReplaceRegion(sublime_plugin.TextCommand):
+    def run(self, edit, *args, **kwargs):
+        selections = [x for x in self.view.sel()]  # deep copy
+        selections = self._run(edit, selections, *args, **kwargs)
+        self.view.sel().clear()
+        for sel in selections:
+            self.view.sel().add(sel)
+
+    def _run(self, edit, selections, r, data, view=None):
+        global ignore_modified_timeout
+
+        if not hasattr(self, 'view'):
+            return selections
+
+        G.IGNORE_MODIFIED_EVENTS = True
+        utils.cancel_timeout(ignore_modified_timeout)
+        ignore_modified_timeout = utils.set_timeout(unignore_modified_events, 2)
+        start = max(int(r[0]), 0)
+        stop = min(int(r[1]), self.view.size())
+        region = sublime.Region(start, stop)
+
+        if stop - start > 10000:
+            self.view.replace(edit, region, data)
+            G.VIEW_TO_HASH[self.view.buffer_id()] = hashlib.md5(sutils.get_text(self.view).encode('utf-8')).hexdigest()
+            return transform_selections(selections, stop, 0)
+
+        existing = self.view.substr(region)
+        i = 0
+        data_len = len(data)
+        existing_len = len(existing)
+        length = min(data_len, existing_len)
+        while (i < length):
+            if existing[i] != data[i]:
+                break
+            i += 1
+        j = 0
+        while j < (length - i):
+            if existing[existing_len - j - 1] != data[data_len - j - 1]:
+                break
+            j += 1
+        region = sublime.Region(start + i, stop - j)
+        replace_str = data[i:data_len - j]
+        self.view.replace(edit, region, replace_str)
+        G.VIEW_TO_HASH[self.view.buffer_id()] = hashlib.md5(sutils.get_text(self.view).encode('utf-8')).hexdigest()
+        new_offset = len(replace_str) - ((stop - j) - (start + i))
+        return transform_selections(selections, start + i, new_offset)
+
+    def is_visible(self):
+        return False
+
+    def is_enabled(self):
+        return True
+
+    def description(self):
+        return
+
+
+# The new ST3 plugin API sucks
+class FlooViewReplaceRegions(FlooViewReplaceRegion):
+    def run(self, edit, commands):
+        is_read_only = self.view.is_read_only()
+        self.view.set_read_only(False)
+        selections = [x for x in self.view.sel()]  # deep copy
+        for command in commands:
+            selections = self._run(edit, selections, **command)
+
+        self.view.set_read_only(is_read_only)
+        self.view.sel().clear()
+        for sel in selections:
+            self.view.sel().add(sel)
+
+    def is_visible(self):
+        return False
+
+    def is_enabled(self):
+        return True
+
+    def description(self):
+        return
