Floobits/floobits-sublime

commit ce6f7c16d662a4618116bb321987dba2bdfe62a7

Tweaks OS X version detection so it doesn't choke on 10.10.

floobits.py
@@ -16,7 +16,7 @@ elif sublime.platform() == 'osx':
     try:
         p = subprocess.Popen(['/usr/bin/sw_vers', '-productVersion'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         result = p.communicate()
-        if float(result[0][:4]) < 10.7:
+        if float(result.split('.')[1]) < 7:
             sublime.error_message('''Sorry, but the Floobits plugin doesn\'t work on 10.6 or earlier.
 Please upgrade your operating system if you want to use this plugin. :(''')
     except Exception as e:

commit d95296371db1186b5b9657f010faac606d18b47d

Fix bug introduced in ce6f7c16d662a4618116bb321987dba2bdfe62a7. @phyllisstein

floobits.py
@@ -16,7 +16,7 @@ elif sublime.platform() == 'osx':
     try:
         p = subprocess.Popen(['/usr/bin/sw_vers', '-productVersion'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         result = p.communicate()
-        if float(result.split('.')[1]) < 7:
+        if float(result[0].split('.')[1]) < 7:
             sublime.error_message('''Sorry, but the Floobits plugin doesn\'t work on 10.6 or earlier.
 Please upgrade your operating system if you want to use this plugin. :(''')
     except Exception as e: