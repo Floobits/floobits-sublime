d7fefc82d76402d171153fa443ba56399ed1de40
Geoff Greer <geoff@greer.fm>
2014-07-02 14:07:37 -0700

    Change msg.error to editor.error_message if we try to connect to a nonexistent workspace.

--- floo/common/flooui.py
+++ floo/common/flooui.py
@@ -199,7 +199,7 @@ class FlooUI(event_emitter.EventEmitter):
 
         res = api.get_workspace(host, owner, workspace)
         if res.code == 404:
-            msg.error("The workspace https://%s/%s/%s does not exist" % (host, owner, workspace))
+            editor.error_message("The workspace https://%s/%s/%s does not exist" % (host, owner, workspace))
             return
 
         if self.agent:
