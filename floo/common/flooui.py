cecfc11063f812bda15d0dca80b9375b701af3c6
Geoff Greer <geoff@greer.fm>
2014-08-23 08:18:51 -0700

    Pass context to user_dir(). Fixes #205.

--- floo/common/flooui.py
+++ floo/common/flooui.py
@@ -337,7 +337,7 @@ class FlooUI(event_emitter.EventEmitter):
 
     @utils.inlined_callbacks
     def prompt_share_dir(self, context, ask_about_dir, api_args):
-        dir_to_share = yield self.user_dir, 'Directory to share: ', ask_about_dir
+        dir_to_share = yield self.user_dir, context, 'Directory to share: ', ask_about_dir
         if not dir_to_share:
             return
         self.share_dir(context, dir_to_share, api_args)
