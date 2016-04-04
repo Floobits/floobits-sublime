9ae523204dfd2fb0b9898947c0b79fc270aa287f
Geoff Greer <geoff@greer.fm>
2016-04-03 21:43:22 -0700

    More progress on repo detection/updating.

--- floo/common/repo.py
+++ floo/common/repo.py
@@ -1,27 +1,44 @@
 import os
 import stat
-
-try:
-    from urllib.parse import urlparse
-    assert urlparse
-except ImportError:
-    from urlparse import urlparse
+import subprocess
 
 try:
     from . import api, msg
     from . import shared as G
-    assert api and G and msg
+    from .exc_fmt import str_e
+    assert api and G and msg and str_e
 except ImportError:
     import api
     import msg
     import shared as G
+    from exc_fmt import str_e
 
 
 REPO_MAPPING = {
-    '.git': 'git',
-    '.svn': 'svn',
-    '.hg': 'hg',
+    'git': {
+        'dir': '.git',
+        'cmd': ['git', 'remote get-url --push origin'],
+    },
+    'svn': {
+        'dir': '.svn',
+        'cmd': ['svn', ''],
+    },
+    'hg': {
+        'dir': '.hg',
+        'cmd': ['hg', ''],
+    },
 }
+
+
+def detect_repo_type(d):
+    for repo_type, v in REPO_MAPPING.items():
+        repo_path = os.path.join(d, v['dir'])
+        try:
+            s = os.stat(repo_path)
+        except Exception:
+            continue
+        if stat.S_ISDIR(s.st_mode):
+            return repo_type
 
 
 def update(workspace_url, project_dir):
@@ -32,16 +49,21 @@ def update(workspace_url, project_dir):
     data = {
         'type': repo_type,
     }
+    cmd = REPO_MAPPING[repo_type]['cmd']
+    try:
+        p = subprocess.Popen(cmd,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             cwd=project_dir)
+        result = p.communicate()
+        repo_url = result[0]
+        msg.log(repo_type, ' url is ', repo_url)
+        if not repo_url:
+            return
+    except Exception as e:
+        msg.error('Error getting ', repo_type, ' url:', str_e(e))
+        return
+
+    data['url'] = repo_url
     # TODO: catch?
     api.update_workspace(workspace_url, data)
-
-
-def detect_repo_type(d):
-    for k, v in REPO_MAPPING.items():
-        repo_path = os.path.join(d, k)
-        try:
-            s = os.stat(repo_path)
-        except Exception:
-            continue
-        if stat.S_ISDIR(s.st_mode):
-            return v
